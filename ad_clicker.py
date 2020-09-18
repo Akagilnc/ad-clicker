2966ee9bac3b3806056ddd34a4e9b75bfffd3985
DeronLee <akagilnc@gmail.com>
2020-09-18 12:40:05 +0900

    Clean up ignored files

--- ad_clicker.py
+++ ad_clicker.py
@@ -18,7 +18,7 @@ for info in ip_addresses:
     proxyPort = info.get('port')
     proxyMeta = "{}:{}".format(proxyHost, proxyPort)
     print(proxyMeta)
-    # proxyMeta = "127.0.0.1:1081"
+    proxyMeta = "127.0.0.1:1081"
     # options = ChromeOptions()
     # options.add_argument('--proxy-server={}'.format(proxyMeta))
     webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
@@ -32,26 +32,27 @@ for info in ip_addresses:
     # with webdriver.Firefox() as driver:
     # Open URL
     fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.headless = True
+    # fireFoxOptions.headless = True
     try:
         with webdriver.Firefox(options=fireFoxOptions) as browser:
             browser.implicitly_wait(5)
             print('open google')
-            browser.get(targetUrl)
+            # browser.get(targetUrl)
+            browser.get('https://www.expressvpn.com/what-is-my-ip')
             search_input_element = browser.find_element_by_css_selector('.gLFyf')
-            time.sleep(random.randint(1, 3))
+            time.sleep(random.randint(1, 5))
             print('enter keyword')
             for letter in 'eyecolens':
                 search_input_element.send_keys(letter)
-                time.sleep(random.uniform(0.2, 1))
+                time.sleep(random.uniform(0.2, 1.5))
             search_input_element.send_keys(Keys.ENTER)
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(1, 12))
             print('find website')
             ad_element = browser.find_element_by_xpath('//a[@href="http://eyecolens.com/"]')
-            time.sleep(random.uniform(1, 4))
+            time.sleep(random.uniform(1, 10))
             print('go to website')
             ad_element.click()
-            time.sleep(random.uniform(4, 10))
+            time.sleep(random.uniform(4, 20))
             print('finished with {}'.format(proxyMeta))
     except Exception as inst:
         print(type(inst))
