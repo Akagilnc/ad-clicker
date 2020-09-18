a0c34952ad7a96027221e2c588ea82673b963825
DeronLee <akagilnc@gmail.com>
2020-09-18 12:56:29 +0900

    change sleep time

--- ad_clicker.py
+++ ad_clicker.py
@@ -35,21 +35,21 @@ for info in ip_addresses:
     fireFoxOptions.headless = True
     try:
         with webdriver.Firefox(options=fireFoxOptions) as browser:
-            browser.implicitly_wait(5)
+            browser.implicitly_wait(8)
             print('open google')
             browser.get(targetUrl)
             # browser.get('https://www.expressvpn.com/what-is-my-ip')
             search_input_element = browser.find_element_by_css_selector('.gLFyf')
-            time.sleep(random.randint(1, 5))
+            time.sleep(random.uniform(1, 5))
             print('enter keyword')
             for letter in 'eyecolens':
                 search_input_element.send_keys(letter)
                 time.sleep(random.uniform(0.2, 1.5))
             search_input_element.send_keys(Keys.ENTER)
-            time.sleep(random.uniform(1, 12))
+            time.sleep(random.uniform(6, 15))
             print('find website')
             ad_element = browser.find_element_by_xpath('//a[@href="http://eyecolens.com/"]')
-            time.sleep(random.uniform(1, 10))
+            time.sleep(random.uniform(3, 10))
             print('go to website')
             ad_element.click()
             time.sleep(random.uniform(4, 20))
