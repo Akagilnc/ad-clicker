bf50a2c3a255b2788c9f83f142f6fcbc9a43e6bf
DeronLee <akagilnc@gmail.com>
2020-12-07 00:31:54 +0900

    add uk branch

--- ad_clicker.py
+++ ad_clicker.py
@@ -1,26 +1,42 @@
 import time
 import random
 import json
-from selenium.webdriver import Chrome
-from selenium.webdriver import ChromeOptions
-import get_daily_ip_addresses
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-get_daily_ip_addresses.get_ip_address()
 # 请求地址
 targetUrl = "https://www.google.com"
-
+i = 0
 # 代理服务器
-ip_addresses = json.load(open('./ip.json', encoding='utf-8')).get('data')
-for info in ip_addresses:
-    proxyHost = info.get('ip')
-    proxyPort = info.get('port')
-    proxyMeta = "{}:{}".format(proxyHost, proxyPort)
+ip_addresses = open('ip.txt', encoding='utf_8')
+hrefs = ['https://tktxinuk.com/',
+         'https://tktxdirect.com/',
+         'https://tktxink.com/']
+fireFoxOptions = webdriver.FirefoxOptions()
+fireFoxOptions.headless = True
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--proxy-server={}'.format("2001:470:1:518::c928:63a6:443"))
+
+
+def agree_click(browser_input):
+    agree_ele = browser_input.find_elements_by_css_selector('.bErdLd')
+    if len(agree_ele) > 0:
+        iframe = browser_input.find_element_by_css_selector('#cnsw > iframe:nth-child(1)')
+        browser_input.switch_to.frame(iframe)
+        browser_input.implicitly_wait(1)
+        agree_btn = browser_input.find_elements_by_css_selector('#introAgreeButton')
+        if len(agree_btn) > 0:
+            agree_btn[0].click()
+            time.sleep(random.uniform(0, 1))
+            browser_input.switch_to.default_content()
+
+
+not_found_list = []
+for proxyMeta in ip_addresses:
     print(proxyMeta)
-    # proxyMeta = "127.0.0.1:1081"
-    # options = ChromeOptions()
-    # options.add_argument('--proxy-server={}'.format(proxyMeta))
+    is_clicked = False
+    # proxyMeta = '127.0.0.1:7890'
+    ip = proxyMeta.split(':')[0]
     webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
         "httpProxy": proxyMeta,
         "ftpProxy": proxyMeta,
@@ -28,32 +44,68 @@ for info in ip_addresses:
         "proxyType": "MANUAL",
 
     }
-
-    # with webdriver.Firefox() as driver:
-    # Open URL
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.headless = True
+    browser = webdriver.Firefox(options=fireFoxOptions)
     try:
-        with webdriver.Firefox(options=fireFoxOptions) as browser:
-            browser.implicitly_wait(10)
-            print('open google')
-            browser.get(targetUrl)
-            search_input_element = browser.find_element_by_css_selector('.gLFyf')
-            time.sleep(random.uniform(2, 8))
-            print('enter keyword')
-            for letter in 'TKTX':
-                search_input_element.send_keys(letter)
-                time.sleep(random.uniform(0.2, 2))
-            search_input_element.send_keys(Keys.ENTER)
-            time.sleep(random.uniform(6, 20))
-            print('find website')
-            href = ['https://tattootktx.com/', 'https://tknumbing.com/'][random.randint(0, 1)]
-            ad_element = browser.find_element_by_xpath('//a[@data-pcu="{}"]'.format(href))
-            time.sleep(random.uniform(3, 10))
+        browser.implicitly_wait(10)
+        print('open google')
+        browser.get(targetUrl)
+        search_input_element = browser.find_element_by_css_selector('.gLFyf')
+        agree_click(browser)
+        time.sleep(random.uniform(2, 15))
+        print('enter keyword')
+        for letter in 'TKTX':
+            search_input_element.send_keys(letter)
+            time.sleep(random.uniform(0.2, 3))
+        search_input_element.send_keys(Keys.ENTER)
+        time.sleep(random.uniform(4, 40))
+        #
+        agree_click(browser)
+        #
+        for href in hrefs:
+            is_page2 = False
+            print('find website {}'.format(href))
+            ad_elements = browser.find_elements_by_xpath('//a[@data-pcu="contains(text(), {})"]'.format(href))
+            time.sleep(random.uniform(3, 20))
+            if len(ad_elements) == 0:
+                page2 = browser.find_element_by_css_selector('#pnnext')
+                page2.click()
+                print('go to page 2')
+                is_page2 = True
+                time.sleep(random.uniform(2, 20))
+                ad_elements = browser.find_elements_by_xpath('//a[@data-pcu="{}"]'.format(href))
             print('go to website {}'.format(href))
-            ad_element.click()
-            time.sleep(random.uniform(10, 30))
+            if len(ad_elements) == 0:
+                print('not found')
+                browser.back()
+                continue
+            ad_elements[0].click()
+            i += 1
+            is_clicked = True
+            time.sleep(random.uniform(4, 16))
+            browser.back()
+            time.sleep(random.uniform(0, 1))
+            if is_page2:
+                browser.back()
             print('finished with {} with {}'.format(proxyMeta, href))
+
+        time.sleep(2)
+
+        if not is_clicked:
+            not_found_list.append(proxyMeta + '\n')
+            browser.set_window_size(1920, 1080)
+            browser.save_screenshot("{}_not_found.png".format(ip))
+        browser.close()
+        time.sleep(random.uniform(15, 50))
     except Exception as inst:
         print(type(inst))
         print(inst)
+        browser.set_window_size(1920, 1080)
+        browser.save_screenshot("{}_screenshot.png".format(ip))
+        browser.close()
+        time.sleep(random.uniform(0, 1))
+    finally:
+        print('clicked {} times'.format(str(i)))
+        browser.quit()
+
+with open('not_found.txt', 'w', encoding='utf_8') as file:
+    file.writelines(not_found_list)
--- get_daily_ip_addresses.py
+++ get_daily_ip_addresses.py
@@ -2,11 +2,14 @@ import requests
 import json
 
 
-def get_ip_address(num=130, regions='us', n_type=0, ip_type=1, data_type=2, lb=1):
-    resp = requests.get('http://tiqu.linksocket.com:81/abroad?num={}&type={}&regions={}&lb={}&flow=1'.format(
-        num, data_type, regions, lb
-    ))
-
-    data_file = open('./ip.json', 'w', encoding='utf-8')
-    json.dump(resp.json(), data_file, indent=4, ensure_ascii=False)
+def get_ip_address(num=300, regions='us', n_type=0, ip_type=1, data_type=2, lb=1):
+    # resp = requests.get(
+    #     'https://www.sharedproxies.com/api.php?m=akagilnc%40gmail.com&s=faaf62382cfd1a3e19c53e955469efceb91cef9b&do=getall')
+    with open('ip.txt', encoding='utf_8') as file:
+        data_file = open('./ip.json', 'w', encoding='utf-8')
+        ip_list = []
+        for info in file:
+            ip, port = info.strip().split(':')
+            ip_list.append({'ip': ip, 'port': port})
+    json.dump(ip_list, data_file, indent=4, ensure_ascii=False)
 
